80f8966a71d8a0d50d827e513cf1d4d0109f2e2b
kkmax-develop <kkmaxdevelop@gmail.com>
2020-09-14 19:58:04 +0900

    ACへ変更

--- src/ABC178/C.java
+++ src/ABC178/C.java
@@ -1,6 +1,5 @@
 package ABC178;
 
-import java.math.*;
 import java.util.*;
 
 public class C {
@@ -11,17 +10,25 @@ public class C {
         sc = new Scanner(System.in);
         long s = sc.nextLong();
         sc.close();
-        BigDecimal ans = new BigDecimal(2);
-        BigDecimal bigDec1 = new BigDecimal(10);
-        BigDecimal bigDec2 = new BigDecimal(Math.pow(10, 9) + 7);
-        int powNum = (int)s - 2;
 
-        if (s != 2) {
-            bigDec1 = bigDec1.pow(powNum);
-            ans = bigDec1.remainder(bigDec2);       
+        long mod = (long) Math.pow(10, 9) + 7;
+        long all = powmod(10, s, mod);
+        long notexist0 = powmod(9, s, mod);
+        long notexist9 = powmod(9, s, mod);
+        long notexistBoth = powmod(8, s, mod);
+        long notexistOne = (notexist0 + notexist9 - notexistBoth);
+        long total = (all - notexistOne) % mod;
+
+        System.out.println((total + mod)%mod);
+
+    }
+
+    public static long powmod(long x, long y, long mod) {
+        long total = 1;
+        for (int i = 0; i < y; i++) {
+            total = (total*x) % mod;
         }
-
-        System.out.println(ans);
-
+        return total;
+        
     }
 }
