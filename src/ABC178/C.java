00bb1c7f482252ef18ee8113195f903d2b328bcf
kkmax-develop <kkmaxdevelop@gmail.com>
2020-09-14 21:26:56 +0900

    Cへコメントを追記

--- src/ABC178/C.java
+++ src/ABC178/C.java
@@ -12,13 +12,26 @@ public class C {
         sc.close();
 
         long mod = (long) Math.pow(10, 9) + 7;
+
+        // 全ての組み合わせ数
         long all = powmod(10, s, mod);
+
+        // 0がない数列の数
         long notexist0 = powmod(9, s, mod);
+
+        // 9がない数列の数
         long notexist9 = powmod(9, s, mod);
+
+        // 0、9が両方ともない数列の数
         long notexistBoth = powmod(8, s, mod);
+
+        // 0、9少なくとも一方は存在しない配列の数
         long notexistOne = (notexist0 + notexist9 - notexistBoth);
+
+        // 全ての組み合わせ - 0、9少なくとも一方は存在しない配列の数 = 0、9がともに存在する配列の数
         long total = (all - notexistOne) % mod;
 
+        // Javaは負の数の剰余が負の数で返ってきてしまう為、正の数で返すためにmodを足してもう一度剰余を出す
         System.out.println((total + mod)%mod);
 
     }
