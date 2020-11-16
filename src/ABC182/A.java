13c021dec70ba706a0127b65a1af5efc15b25a05
kkmax-develop <kkmaxdevelop@gmail.com>
2020-11-16 09:43:46 +0900

    First Commit

---
+++ src/ABC182/A.java
@@ -0,0 +1,20 @@
+package ABC182;
+
+import java.util.*;
+
+public class A {
+    public static void main(String[] args){
+ 
+        Scanner sc = null;
+        sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        sc.close();
+
+        if (x >= 0) {
+            System.out.println(x);
+          } else {
+            System.out.println(0);
+          }
+      
+    }
+}
---
+++ src/ABC182/B.java
@@ -0,0 +1,33 @@
+package ABC182;
+
+import java.util.*;
+
+public class B {
+    public static void main(String[] args){
+ 
+        Scanner sc = null;
+        sc = new Scanner(System.in);
+        double sx = sc.nextDouble();
+        double sy = sc.nextDouble();
+        double gx = sc.nextDouble();
+        double gy = sc.nextDouble();
+        sc.close();
+
+        double ans = 0;
+
+        // G座標の反転
+        double gy2 = gy * -1;
+
+        // 傾きの算出
+        double slope = (gy2 - sy) / (gx - sy);
+
+        // 切片の算出
+        double intercept = sy - (slope * sx);
+
+        // ｙ = 0となるx座標を算出
+        ans = (intercept * -1) * (1 / slope);
+
+        System.out.println(ans);
+      
+    }
+}
