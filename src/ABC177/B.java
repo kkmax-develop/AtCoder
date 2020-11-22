7fc7ba05abd046d420f59b2df9dacac58c72cc40
kkmax-develop <kkmaxdevelop@gmail.com>
2020-11-22 18:46:48 +0900

    BをACに修正

--- src/ABC177/B.java
+++ src/ABC177/B.java
@@ -9,33 +9,24 @@ public class B {
         Scanner sc = null;
         sc = new Scanner(System.in);
         String s = sc.next();
-        String n = sc.next();
+        String t = sc.next();
         sc.close();
-        int ans = n.length();
-        String subn = null;
-        int index = 0;
-        String work_s = null;
-        String work_n = null;
-        String work_sb = null;
-        String work_nb = null;
-        for (int i = 0; i < n.length(); i++) {
-            for (int j = i+1; j <= n.length(); j++) {
-                subn = n.substring(i, j);
-                if (s.contains(subn)) {
-                    index = s.indexOf(subn) + 1;
-                    work_s = s.substring(0, index);
-                    work_n = n.substring(0, i);
-                    work_sb = s.substring(index, s.length());
-                    work_nb = n.substring(i, n.length());
-                    if (work_s.length() >= work_n.length() && work_sb.length() >= work_nb.length()) {
-                        if (ans > n.length() - subn.length()) {
-                            ans = n.length() - subn.length();
-                        }
+        int ans = t.length();
+        int diff = 0;
+
+        if (s.length() >= t.length()) {
+            for (int i = 0; i <= s.length() - t.length(); i++) {
+                for (int j = 0; j < t.length(); j++) {
+                    if (t.charAt(j) != s.charAt(i+j)) {
+                        diff += 1;
                     }
-                } else {
-                    break;
                 }
-            }
+                ans = Math.min(ans, diff);
+                diff = 0;
+    
+            }         
+        } else {
+            ans = 0;
         }
         System.out.println(ans);
 
