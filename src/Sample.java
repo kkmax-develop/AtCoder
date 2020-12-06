3a2ab02a399eb74f13267abac0c0879083096a6c
kkmax-develop <kkmaxdevelop@gmail.com>
2020-12-06 23:53:36 +0900

    新規ファイルのテンプレート追加

---
+++ src/Sample.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        Scanner sc = null;
+        sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        long n = sc.nextLong();
+        String s = sc.next();
+        sc.close();
+        int ans = 0;
+
+        System.out.println(ans);
+
+    }
+}
